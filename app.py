92926411b50a750725e1cbe226bf54b0bcc99d8c
Marius Just <mjust@econ.au.dk>
2025-10-29 15:40:27 +0100

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template
-import os
-
 
 app = Flask(__name__)
 app.config['EXPLAIN_TEMPLATE_LOADING'] = True 
@@ -14,3 +12,7 @@ def index():
 @app.route('/neuralnet')
 def neuralnet():
     return render_template("NeuralNet.html")
+
+@app.route('/DGPE_Køge')
+def dgpe_koege():
+    return render_template("DGPE.html")
